1b1b5579f4d5b54b514b8a7ad84356db5e136bae
Steven Liou <stevenliou@gmail.com>
2022-01-26 18:08:07 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolor sed,
+        totam aliquam laborum illum saepe sequi harum temporibus excepturi
+        molestiae non. Eaque totam obcaecati iure accusantium provident, nulla
+        rem sed placeat commodi fugiat. Commodi nostrum est ratione aliquid
+        debitis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
